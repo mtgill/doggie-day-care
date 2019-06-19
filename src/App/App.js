396da70b00e4aec6640dccf5ece2305e22010610
Matt Gill <mtgill90@outlook.com>
2019-06-18 20:56:51 -0500

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <button className="btn btn-warning">Button</button>
+    );
+  }
+}
+
+export default App;
