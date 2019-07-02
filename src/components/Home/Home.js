48a3c5c64546a045caed2862942c3a779ff7808e
Matt Gill <mtgill90@outlook.com>
2019-07-02 16:46:31 -0500

    wip

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
-
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import './Home.scss';
 
 import DogPen from '../Dog/DogPen/DogPen';
+import dogData from '../../helpers/data/dogData';
 import StaffRoom from '../StaffRoom/StaffRoom';
+import employeeData from '../../helpers/data/employeeData';
 import walkData from '../../helpers/data/walkData';
 import Walk from '../Walk/Walk';
 
 class Home extends React.Component {
   state = {
+    dogs: [],
+    employees: [],
     walks: [],
   }
 
@@ -19,6 +23,13 @@ class Home extends React.Component {
   }
 
   componentDidMount() {
+    dogData.getDogs()
+      .then(dogs => this.setState({ dogs }))
+      .catch(err => console.error('no dogs available', err));
+    employeeData.getemployees()
+      .then(employees => this.setState({ employees }))
+      .catch(err => console.error('no eomployees available', err));
+
     this.getWalks();
   }
 
@@ -29,15 +40,36 @@ class Home extends React.Component {
   }
 
   render() {
+    const { dogs, employees } = this.state;
     const walkComponents = this.state.walks.map(walk => (
       <Walk key={walk.id} walk={walk} deleteWalks={this.deleteWalks}/>
     ));
+    const dogOptions = this.state.dogs.map(dog => (
+      <option key={dog.id}>{dog.name}</option>
+    ));
+    const employeeOptions = this.state.employees.map(employee => (
+      <option key={employee.id}>{employee.name}</option>
+    ));
     return (
       <div className="Home">
+        <Form>
+        <FormGroup>
+          <Label for="exampleSelect">Select a Dog</Label>
+          <Input type="select" name="select" id="exampleSelect">
+            {dogOptions}
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleSelect">Select an Employee</Label>
+          <Input type="select" name="select" id="exampleSelect">
+            {employeeOptions}
+          </Input>
+        </FormGroup>
+        </Form>
       <div><h2>Dogs</h2></div>
-      <DogPen />
+      <DogPen dogs={ dogs }/>
       <div><h2>Employees</h2></div>
-      <StaffRoom />
+      <StaffRoom employees={ employees }/>
       <div className="row">{walkComponents}</div>
     </div>
     );
